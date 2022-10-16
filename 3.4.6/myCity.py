5e2e29f02e6dfdf6b293d26f40b76c66cd713221
Xilth <el.jason156@gmail.com>
2022-10-16 06:41:11 +0000

    myCity and myLocation Python File

---
+++ 3.4.6/myCity.py
@@ -0,0 +1,6 @@
+def myCity(cityName):
+    print("I live in "+ cityName + ".")
+
+myCity("Austin")
+myCity("Tokyo")
+myCity("Salzburg")  
---
+++ 3.4.6/myLocation.py
@@ -0,0 +1,6 @@
+class Location:
+    def __init__(self, name, country):
+        self.name = name
+        self.country = country
+
+loc = Location("Jason Elington", "Indonesia")
