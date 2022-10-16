b3af00b537ee423dc0be1ed06b04607d82cd8f2f
Xilth <el.jason156@gmail.com>
2022-10-16 06:46:54 +0000

    myLocation.py myLocation method added

--- 3.4.6/myLocation.py
+++ 3.4.6/myLocation.py
@@ -2,5 +2,15 @@ class Location:
     def __init__(self, name, country):
         self.name = name
         self.country = country
+    
+    def myLocation(self):
+        print("Hi, my name is "+ self.name + " and I live in "+ self.country + ".")
 
 loc = Location("Jason Elington", "Indonesia")
+print(loc.name)
+print(loc.country)
+
+print(type(loc))
+print("------------")
+loc2 = Location("Jason Elington", "Indonesia")
+loc2.myLocation()
