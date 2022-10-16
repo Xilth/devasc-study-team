70310dba03b3ae813cebeb8cf0727f3e76218c47
Xilth <el.jason156@gmail.com>
2022-10-16 07:42:24 +0000

    3.5.7 progress halted, res is diff from .pdf

--- 3.5.7/unittest/test_json_search.py
+++ 3.5.7/unittest/test_json_search.py
@@ -1,1 +1,22 @@
-# Fill the Python code in this file
+import unittest
+
+from recursive_json_search import *
+from test_data import *
+
+class json_search_test(unittest.TestCase):
+    def test_search_found(self):
+        self.assertTrue([]!=json_search(key1,data))
+    def test_search_not_found(self):
+        self.assertTrue([]==json_search(key2,data))
+    def test_is_a_list(self):
+        self.assertIsInstance(json_search(key1,data),list)
+    ret_val = []
+    def json_search(key, input_object):
+        if isinstance(input_object, dict):
+            for k, v in input_object.items():
+                if k == key:
+                    temp = {k:v}
+                    ret_val.append(temp)
+    def __main__():
+        if __name__ == '__main__':
+            unittest.main()
