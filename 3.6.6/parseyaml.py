48d4295404220a42cffe8a7f7d50dce0f01ba722
Xilth <el.jason156@gmail.com>
2022-10-16 13:02:09 +0000

    parseyaml.py finished

--- 3.6.6/parseyaml.py
+++ 3.6.6/parseyaml.py
@@ -1,1 +1,13 @@
-# Fill in this file with the code from parsing YAML exercise
+import json
+import yaml
+
+with open('myfile.yaml', 'r') as yaml_file:
+    ourYAML = yaml.safe_load(yaml_file)
+
+print(ourYAML)
+
+print("The access token is {}".format(ourYAML['access_token']))
+print("The token expires in {}".format(ourYAML['expires_in'])+" seconds")
+
+print("\n\n")
+print(json.dumps(ourYAML, indent=4))
